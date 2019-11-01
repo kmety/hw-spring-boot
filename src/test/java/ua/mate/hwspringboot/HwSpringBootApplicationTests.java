514d8463415e6de5bf70d05b8d89ecdadca04fe0
kmety <kmety9876@gmail.com>
2019-11-01 22:26:50 +0200

    first commit

---
+++ src/test/java/ua/mate/hwspringboot/HwSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mate.hwspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
